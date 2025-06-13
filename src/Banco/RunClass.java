7fc7878cdc73a4f3767b17e58ed56dc37cf58c43
Juan <juanmartinscaitano@gmail.com>
2025-06-13 18:28:41 -0300

    The beggining

---
+++ src/Banco/RunClass.java
@@ -0,0 +1,15 @@
+package Banco;
+
+public class RunClass {
+    public static void main(String[] args){
+        System.out.println("Inicio do projeto");
+    }
+}
+
+
+/* To-do List
+* Atributos: conta, agência, nome do cliente, nascimento, saldo*
+
+  Métodos:Sacar, Transferir, Cancelar conta com justificativa, Consultar o
+  * extrato entre duas datas, consultar saldo atual.
+ */
